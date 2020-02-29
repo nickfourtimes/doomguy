1a547c0f49b07457ff5d6e8fb298de7c2b84372d
nrudzicz <nrudzicz@gmail.com>
2020-02-29 13:40:08 -0500

    first commit

---
+++ Assets/Scripts/Doomguy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doomguy : MonoBehaviour {
+
+	public float refreshInterval = 1f;
+
+	IEnumerator Start() {
+		var animComp = GetComponent<Animator>();
+
+		while(true) {
+			if(Input.GetKeyDown(KeyCode.Escape)) {
+				break;
+			}
+
+			animComp.SetFloat("doomguy", Random.value);
+			yield return null;
+			animComp.SetFloat("doomguy", 0.5f);
+
+			yield return new WaitForSeconds(refreshInterval);
+		}
+
+		yield break;
+	}
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+## introduction
+it's doomguy!
+
+what's around that corner??
+
+rarrrr! it's a monster!
+
+## roadmap
+### ideas for future releases
+* different levels of damage
+* iddqd
+* idkfa
